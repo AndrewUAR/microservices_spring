032c09561910857ebd923d51a917be9a789d7058
Andrew <andrewriznykua93@gmail.com>
2020-11-05 12:46:08 -0600

    first commit

---
+++ PhotoAppApiAccountManagment/src/main/java/com/appdeveloper/photoapp/api/account/PhotoAppApiAccountManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.appdeveloper.photoapp.api.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiAccountManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiAccountManagmentApplication.class, args);
+	}
+
+}
