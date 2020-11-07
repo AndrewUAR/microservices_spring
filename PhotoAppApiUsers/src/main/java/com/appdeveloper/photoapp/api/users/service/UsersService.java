3a6c5710cb825be37220db82dbabfb9a281ae6d5
Andrew <andrewriznykua93@gmail.com>
2020-11-06 22:16:20 -0600

    users service created

--- PhotoAppApiUsers/src/main/java/com/appdeveloper/photoapp/api/users/service/UsersService.java
+++ PhotoAppApiUsers/src/main/java/com/appdeveloper/photoapp/api/users/service/UsersService.java
@@ -1,7 +1,10 @@
 package com.appdeveloper.photoapp.api.users.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.appdeveloper.photoapp.api.users.shared.UserDto;
 
-public interface UsersService {
+public interface UsersService extends UserDetailsService {
 	UserDto create(UserDto userDetails);
+	UserDto getUserDetailsByEmail(String email);
 }
